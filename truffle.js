73773d70724396b36ae16f1be9c76ac11b97d6bf
kylehz <kylehz0617@gmail.com>
2019-10-23 15:25:09 +0800

    update

--- truffle.js
+++ truffle.js
@@ -1,9 +1,43 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+
 module.exports = {
-  networks : {
-    ganache : {
-      host : 'localhost',
-      port : 7545,    // By default Ganache runs on this port.
-      network_id : "*" // network_id for ganache is 5777. However, by keeping * as value you can run this node on  any network
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*' // Match any network id
+    },
+    // testnets
+    // properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
     }
   },
   compilers: {
@@ -19,4 +53,4 @@ module.exports = {
       // }
     }
   }
-};
+}
